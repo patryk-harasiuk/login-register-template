c48152044753f4cb8d3f56a8e6f154adab093c27
Patryk Harasiuk <patryk.harasiuk@op.pl>
2022-02-26 23:34:15 +0100

    add basic route service

--- backend/app/index.ts
+++ backend/app/index.ts
@@ -1,8 +1,18 @@
 import express from "express";
 
+import routes from "./routes";
+
 const app = express();
 
 app.use(express.json({ strict: true }));
+
+for (let route of routes) {
+  app[route.method.toLocaleLowerCase()]((res, req, next) => {
+    console.log(req.url);
+
+    res.send("ok");
+  });
+}
 
 app
   .listen(4000, () => {
